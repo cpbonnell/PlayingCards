2aac78ac5843f9304bbd7c4334f812e652def9f5
cpbonnell <christian.bonnell@gmail.com>
2017-05-12 20:36:36 -0500

    Added new sub-package and class for doing bitmask operations.

---
+++ src/com/cpbonnell/cards/Utils/BitmaskOperations.java
@@ -0,0 +1,15 @@
+package com.cpbonnell.cards.Utils;
+
+/**
+ * Created by MYE7180 on 5/12/2017.
+ */
+public abstract class BitmaskOperations {
+
+    // Make non-instantiable
+    private BitmaskOperations() {
+        
+    }
+
+    // Functions relating to card number
+    
+}
